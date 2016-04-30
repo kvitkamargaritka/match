8fbbc985cdfdc49d42e68d7dca939ff531a403fc
Margarita <rita.akhmadulina@gmail.com>
2016-04-30 18:25:33 +0300

    add new styles html and js

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
 
-	// go js
+  //ddslick initialisation
+
+  if ($('#select1').length >= 1) {
+    $('#select1').ddslick({
+      width: 170,
+      onSelected: function (selectedData) {
+        //callback function: do something with selectedData;
+      }
+    });
+  }
+
+  if ($('#select2').length >= 1) {
+    $('#select2').ddslick({
+      width: 200,
+      onSelected: function (selectedData) {
+        //callback function: do something with selectedData;
+      }
+    });
+  }
+
+  if ($('#select3').length >= 1) {
+    $('#select3').ddslick({
+      height: 230,
+      onSelected: function (selectedData) {
+        //callback function: do something with selectedData;
+      }
+    });
+  }
+
+  if ($('#select4').length >= 1) {
+    $('#select4').ddslick({
+      //width: 170,
+      onSelected: function (selectedData) {
+        //callback function: do something with selectedData;
+      }
+    })
+  }
+
+
+  //click's
+  $('#btn-acc-info').click(function () {
+    $(this).parent().find('.drop-down').toggle();
+  });
+
+  $('#btn-data-range').click(function () {
+    $(this).parent().find('.drop-down').toggle();
+  });
 
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/ddslick.js
 //=include common.js
